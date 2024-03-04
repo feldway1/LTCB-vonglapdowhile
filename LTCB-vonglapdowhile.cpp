77b5702d2a6da2c8c05badc210c1b19b76cb5e83
ADMIN <ADMIN@LAB1-MAY13>
2024-03-04 08:57:53 +0700

    Add project files.

---
+++ LTCB-vonglapdowhile.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int sodau, socuoi;
+	cout << "Nhap so dau:";
+	cin >> sodau;
+	cout << "Nhap so cuoi:";
+	cin >> socuoi;
+	cout << "VONG LAP WHILE\n";
+	while (sodau <= socuoi) {
+		cout << sodau << " ";
+		sodau++;
+	}
+	cout << "\nNhap so dau:";
+	cin >> sodau;
+	cout << "Nhap so cuoi:";
+	cin >> socuoi;
+	cout << "VONG LAP DO WHILE\n";
+	do {
+		cout << sodau << " ";
+		sodau++;
+	} while (sodau <= socuoi);
+	return 0;
+}
+
+		
